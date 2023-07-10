bd11c7c44b19e3c5334be81920abf8ad1d34316e
Amin Affandi <amin.affandi98@gmail.com>
2023-07-11 00:32:51 +0800

    <UPDATE> Aim lab first level operation

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class Circle:
     def draw(self, x, y, w, h):
         pygame.draw.ellipse(window, self.color, (x, y, w, h))
 
-def display_text(font, w, h, window, text ):
-    text_surface = font.render(text, True, (0, 0, 0))
+def display_text(font, font_color, w, h, window, text ):
+    text_surface = font.render(text, True, font_color)
     text_rect = text_surface.get_rect(center=(w,h))
     window.blit(text_surface, text_rect)
 
@@ -25,6 +25,7 @@ clock = pygame.time.Clock()
 running = True
 game_running = False
 start = True
+score = 0
 
 # timer setup
 duration = 11 * 1000
@@ -46,6 +47,7 @@ mouse_x, mouse_y = (window_width // 2, window_height // 2)
 
 # Set up the font
 font = pygame.font.SysFont(None, 48)
+font_color = (182, 192, 207)
 
 circle = Circle( circle_color)
 
@@ -57,13 +59,13 @@ while running:
             running = False
 
     # fill the window with a color to wipe away anything from last frame
-    window.fill("purple")
+    window.fill((36, 38, 41))
 
     # RENDER YOUR GAME HERE
     # start game
     if start:
         current_time = pygame.time.get_ticks()
-        display_text(font, window_width/2, window_height/6, window, text="Click on ball to start game!")
+        display_text(font, font_color, window_width/2, window_height/6, window, text="Click on ball to start game!")
         cointaner_x_start = window_width/2
         cointaner_y_start = window_height/2
         circle.draw(cointaner_x_start, cointaner_y_start, container_width, container_height)
@@ -78,7 +80,7 @@ while running:
                         print("Game started!")
                         game_running = True
                         start = False
-        window.fill('purple')
+        window.fill((36, 38, 41))
 
 
     if game_running:
@@ -87,7 +89,7 @@ while running:
         remaining_time = max(0, (duration - start_time)) // 1000
         # Render the countdown timer
         if remaining_time != 0:
-            display_text(font, window_width/2, window_height/6, window, text=str(remaining_time))
+            display_text(font, font_color, window_width/2, window_height/6, window, text=str(remaining_time))
             circle.draw(cointaner_x, cointaner_y, container_width, container_height)
             # Event of click on circle
             if event.type == MOUSEBUTTONDOWN:
@@ -101,15 +103,26 @@ while running:
                     cointaner_y = random.randint(constraint_y_top + container_height, constraint_y_bottom - container_height)
                     circle.draw(cointaner_x, cointaner_y, container_width, container_height)
                     print("Ball Hit! +1 Point ")
+                    score += 1
         elif remaining_time <= 0:
-            display_text(font, window_width/2, window_height/5, window, text="Time Reached!")
+            display_text(font, font_color, window_width/2, window_height/5, window, text="Times Up!")
+            display_text(font, font_color, window_width/2, window_height/2, window, text=f'Your Score : {str(score)}')
+    
+            #TODO 1.0 print score after game finish only once
+            #TODO 2.0 restart game / home option
+            #TODO 3.0 display real time score
+            #TODO 4.0 adjust constraint area for ball to appear
+            #TODO 5.0 add countdown before start game
+            #TODO 6.0 add sound if ball hit and ball appear
+            #TODO 7.0 change mouse cursor
+            #TODO allow player to pick time [10,30,60] seconds
 
     # flip() the display to put your work on window
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
-    
+
 
 pygame.quit()
 
